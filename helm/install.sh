3c7bfce27710d9c861ce10d8278221b2dc5d6a1d
Dr. Rajagopala Medpur Vasantha Reddy <80036288+RajuMedpur@users.noreply.github.com>
2025-10-27 14:06:59 +0530

    Update install.sh

--- helm/install.sh
+++ helm/install.sh
@@ -4,16 +4,12 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add elastic https://helm.elastic.co
 helm repo update
-
 # Install Prometheus + Grafana
 helm install prometheus prometheus-community/kube-prometheus-stack
-
 # Install Elasticsearch
 helm install elasticsearch elastic/elasticsearch
-
 # Install Kibana
 helm install kibana elastic/kibana
-
 # Deploy Fluentd (you can use a custom chart or manifest)
 kubectl apply -f ../manifests/fluentd-configmap.yaml
 kubectl apply -f https://raw.githubusercontent.com/fluent/fluentd-kubernetes-daemonset/master/fluentd-daemonset-elasticsearch-rbac.yaml
